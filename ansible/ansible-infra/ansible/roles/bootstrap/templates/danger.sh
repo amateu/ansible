49aee201d70dade826e3f69c291cab89e76f5f39
qiuq <506324275@qq.com>
2020-11-09 11:36:21 +0800

    ---

---
+++ ansible/ansible-infra/ansible/roles/bootstrap/templates/danger.sh
@@ -0,0 +1,4 @@
+alias reboot='echo "This command is disabled" && echo "`who` `date` Run reboot $@ command" >> /var/log/danger.log'
+alias halt='echo "This command is disabled" && echo "`who` `date` Run halt $@ command" >> /var/log/danger.log'
+alias shutdown='echo "This command is disabled" && echo "`who` `date` Run shutdown $@ command" >> /var/log/danger.log'
+alias poweroff='echo "This command is disabled" && echo "`who` `date` Run poweroff $@ command" >> /var/log/danger.log'
